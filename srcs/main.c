4882096092b38a0c4d4fe26f7b65c60804d9447d
Erwann Millon <erwann@Erwanns-MacBook-Air.local>
2022-10-14 14:10:46 +0200

    I made some changes

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,30 @@
 /*   By: gmillon <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/21 14:56:50 by gmillon           #+#    #+#             */
-/*   Updated: 2022/05/21 14:56:50 by gmillon          ###   ########.fr       */
+/*   Updated: 2022/10/13 21:44:00 by gmillon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(int argc, char **argv)
+#include "philo.h"
+
+void	handle_error(int *vars)
 {
+	free(vars);
+	printf("Error\n");
+	exit(1);
+}
+
+int	main(int argc, char **argv)
+{
+	int		*vars;
+	t_state	state;
+
+	vars = initialize(argc, argv);
+	state = create_state(vars);
+	if (!pthread_mutex_lock(&state.philo_arr[0].fork))
+		printf("AAAAAAAAAAAAAAAAAAAAAa");
+	usleep(1000);
+	join_threads(vars, &state);
 	return (0);
+
 }
